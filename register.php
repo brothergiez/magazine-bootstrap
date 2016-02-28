12c8d25b2f3913b7ba3e0d8b7083fc8bd61cbdd5
brothergiez <giezbungadeza@gmail.com>
2016-02-29 00:12:15 +0700

    form register

---
+++ register.php
@@ -0,0 +1,51 @@
+<?php include "header.php"; ?>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 col-sm-12">
+					<form class="form-signin">
+				        <center><h2 class="form-signin-heading">Lengkapi Data</h2></center>
+				        <label for="inputEmail" class="sr-only">Nama Lengkap</label>
+				        <input type="text" id="inputNama" class="form-control" placeholder="Nama Lengkap" required autofocus>
+				        <label for="inputEmail" class="sr-only">Alamat E-mail</label>
+				        <input type="email" id="inputEmail" class="form-control" placeholder="Alamat Email" required autofocus>
+				        <label for="inputPassword" class="sr-only">Kata Sandi</label>
+				        <input type="password" id="inputPassword" class="form-control" placeholder="Kata Sandi" required>
+				        <input type="submit" class="btn btn-primary btn-block" value="Register"><br>
+				        Sudah punya akun? <a href="login.php">Klik disini untuk login.</a> 
+				     </form>
+				     <div class="ads-slot">
+						<script type='text/javascript'>
+						<!--//<![CDATA[
+						   var m3_u = (location.protocol=='https:'?'https://a01.uadexchange.com/www/delivery/ajs.php':'http://a01.uadexchange.com/www/delivery/ajs.php');
+						   var m3_r = Math.floor(Math.random()*99999999999);
+						   if (!document.MAX_used) document.MAX_used = ',';
+						   document.write ("<scr"+"ipt type='text/javascript' src='"+m3_u);
+						   document.write ("?zoneid=162");
+						   document.write ('&amp;cb=' + m3_r);
+						   if (document.MAX_used != ',') document.write ("&amp;exclude=" + document.MAX_used);
+						   document.write (document.charset ? '&amp;charset='+document.charset : (document.characterSet ? '&amp;charset='+document.characterSet : ''));
+						   document.write ("&amp;loc=" + escape(window.location));
+						   if (document.referrer) document.write ("&amp;referer=" + escape(document.referrer));
+						   if (document.context) document.write ("&context=" + escape(document.context));
+						   if (document.mmm_fo) document.write ("&amp;mmm_fo=1");
+						   document.write ("'><\/scr"+"ipt>");
+						//]]>-->
+						</script><noscript><a href='http://a01.uadexchange.com/www/delivery/ck.php?n=aac0bb79&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='http://a01.uadexchange.com/www/delivery/avw.php?zoneid=162&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=aac0bb79' border='0' alt='' /></a></noscript>
+					</div>					
+				</div>
+			</div>
+		</div>
+
+
+		<footer>
+		<div class="navbar navbar-default footer">
+			<div class="container">
+				<p class="navbar-text pull-left">siapadia.id</p>
+				<p class="navbar-text pull-right">&copy; copyright 2016</p>
+			</div>
+		</footer>
+		</div>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+	</body>
+</html>
