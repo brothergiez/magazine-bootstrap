0a9795492818b33bdfd24e038cf13163a92c65f7
brothergiez <giezbungadeza@gmail.com>
2016-02-29 17:26:00 +0700

    front end with slim

---
+++ admin/application/views/footer.php
@@ -0,0 +1,13 @@
+		<footer>
+		<div class="navbar navbar-default footer">
+			<div class="container">
+				<p class="navbar-text pull-left">siapadia.id</p>
+				<p class="navbar-text pull-right">&copy; copyright 2016</p>
+			</div>
+		</footer>
+		</div>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
+		<script src="<?php echo base_url('assets/js/script.js') ?>"></script>
+	</body>
+</html>
--- index.php
+++ index.php
@@ -1,92 +1,36 @@
-<?php include "header.php"; ?>
-		<!--container -->
-		<div class="container">
-			<div class="row">
-				<div class="col-md-5 col-sm-12">
-					<img src="assets/images/cobain.png" class="img-responsive big-img" >
-				</div>
-				<div class="col-md-3 col-sm-12">
-					<div class="title">
-						<span class="glyphicon glyphicon-bookmark" aria-hidden="true"></span>&nbsp;&nbsp;<a href="detail.php">Kurt Cobain</a>
-					</div>
-					<div class="article">
-						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with...
-					</div>
-				</div>
-				<div class="col-md-4 col-sm-12">
-					<div class="ads-slot">
-						<script type='text/javascript'>
-						<!--//<![CDATA[
-						   var m3_u = (location.protocol=='https:'?'https://a01.uadexchange.com/www/delivery/ajs.php':'http://a01.uadexchange.com/www/delivery/ajs.php');
-						   var m3_r = Math.floor(Math.random()*99999999999);
-						   if (!document.MAX_used) document.MAX_used = ',';
-						   document.write ("<scr"+"ipt type='text/javascript' src='"+m3_u);
-						   document.write ("?zoneid=162");
-						   document.write ('&amp;cb=' + m3_r);
-						   if (document.MAX_used != ',') document.write ("&amp;exclude=" + document.MAX_used);
-						   document.write (document.charset ? '&amp;charset='+document.charset : (document.characterSet ? '&amp;charset='+document.characterSet : ''));
-						   document.write ("&amp;loc=" + escape(window.location));
-						   if (document.referrer) document.write ("&amp;referer=" + escape(document.referrer));
-						   if (document.context) document.write ("&context=" + escape(document.context));
-						   if (document.mmm_fo) document.write ("&amp;mmm_fo=1");
-						   document.write ("'><\/scr"+"ipt>");
-						//]]>-->
-						</script><noscript><a href='http://a01.uadexchange.com/www/delivery/ck.php?n=aac0bb79&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='http://a01.uadexchange.com/www/delivery/avw.php?zoneid=162&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=aac0bb79' border='0' alt='' /></a></noscript>
-					</div>
-					<div class="clearfix"></div>
-				</div>
-			</div>
-		</div>
-		<div class="clearfix"></div>
-		<!--end of container -->
-		<!--container -->
-		<div class="container">
-			<div class="row">
-				<div class="col-md-3 col-sm-12 column">
-					<div class="mdm">
-						<img src="assets/images/zakir-naik.jpg" class="img-responsive medium-img">
-					</div>
-					<div class="title-small">
-						<span class="glyphicon glyphicon-bookmark" aria-hidden="true"></span>&nbsp;&nbsp;<a href="detail.php">Dr. Zakir Naik</a>
-					</div>
-					<div class="article">
-						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-					</div>
-				</div>
-				<div class="col-md-3 col-sm-12 column">
-					<div class="mdm">
-						<img src="assets/images/monroe.jpg" class="img-responsive medium-img">
-					</div>
-					<div class="title-small">
-						<span class="glyphicon glyphicon-bookmark" aria-hidden="true"></span>&nbsp;&nbsp;<a href="detail.php">Marylin Monroe</a>
-					</div>
-					<div class="article">
-						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-					</div>
-				</div>
-				<div class="col-md-3 col-sm-12 column">
-					<div class="mdm">
-						<img src="assets/images/zidane.jpg" class="img-responsive medium-img">
-					</div>
-					<div class="title-small">
-						<span class="glyphicon glyphicon-bookmark" aria-hidden="true"></span>&nbsp;&nbsp;<a href="detail.php">Zinedine Zidane</a>
-					</div>
-					<div class="article">
-						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-					</div>
-				</div>
-				<div class="col-md-3 col-sm-12 column">
-					<div class="mdm">
-						<img src="assets/images/erdogan.jpg" class="img-responsive medium-img">
-					</div>
-					<div class="title-small">
-						<span class="glyphicon glyphicon-bookmark" aria-hidden="true"></span>&nbsp;&nbsp;<a href="detail.php">Recep Tayyip Erdoğan</a>
-					</div>
-					<div class="article">
-						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-					</div>
-				</div>
-			</div>
-		</div>
-		<!--end of container -->
-<?php include "footer.php"; ?>
+<?php
+	require 'vendor/autoload.php';
+
+	$app = new \Slim\Slim(array(
+		"view" => new \Slim\Extras\Views\Twig()
+	));
+
+	$app->base_url = function ()use($app) {
+		$req 		= $app->request();
+		$root_uri 	= $req->getRootUri();
+		return $root_uri;
+	};
+
+	$app->hook('slim.before.dispatch', function () use ($app) {
+		$app->render('/layout/header.php');
+	});
+	  
+	$app->hook('slim.after.dispatch', function () use ($app) {
+		$app->render('/layout/footer.php');
+	});	
+
+	$app->hook('slim.before', function () use ($app) {
+    	$app->view()->appendData(array('baseUrl' => $app->base_url));
+	});
+
+	$app->get('/', function ()use($app){
+		$app->render('/body/index.php');
+	});
+
+	$app->get('/detail', function ()use($app){
+		$app->render('/body/detail.php');
+	});
+
+
+	$app->run();
+?>
